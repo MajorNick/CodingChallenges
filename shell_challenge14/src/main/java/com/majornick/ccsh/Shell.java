11c3a71bc959e82741b206e0775900e02452b9b8
MajorNick <nika.chigladze11@gmail.com>
2024-03-14 18:44:27 +0400

    14th challenge: added pwd,cd,cat cmds

--- shell_challenge14/src/main/java/com/majornick/ccsh/Shell.java
+++ shell_challenge14/src/main/java/com/majornick/ccsh/Shell.java
@@ -2,27 +2,49 @@ package com.majornick.ccsh;
 
 import com.majornick.ccsh.builtin.CommandExecutors;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Shell {
+    private File currentDir;
+
+    public Shell() {
+        currentDir = new File(".");
+    }
+
+    public File getCurrentDir() {
+        return currentDir;
+    }
+
+    public void setCurrentDir(File currentDir) {
+        this.currentDir = currentDir;
+    }
+
     public void start() {
-        String[] args = getNewInputLine();
-        if (args.length == 0) {
-            System.err.println("no input");
-            System.exit(0);
+        while (true) {
+            String[] args = getNewInputLine();
+            if (args.length == 0) {
+                System.err.println("no input");
+                System.exit(0);
+            }
+            switchCmds(args);
         }
-        switchCmds(args);
     }
 
     private String[] getNewInputLine() {
         System.out.print("ccsh> ");
         Scanner scanner = new Scanner(System.in);
-        return scanner.next().split(" ");
+        return scanner.nextLine().split("\\s+");
     }
 
     private void switchCmds(String[] args) {
         switch (args[0]) {
-            case "ls" -> CommandExecutors.execute_ls(args);
+            case "ls" -> CommandExecutors.execute_ls(this, args);
+            case "pwd" -> CommandExecutors.execute_pwd(this);
+            case "cd" -> CommandExecutors.execute_cd(this, args);
+            case "cat" -> CommandExecutors.execute_cat(this, args);
+            case "exit" -> System.exit(0);
+            default -> System.out.println("No such file or directory");
         }
     }
 }
--- shell_challenge14/src/main/java/com/majornick/ccsh/builtin/CommandExecutors.java
+++ shell_challenge14/src/main/java/com/majornick/ccsh/builtin/CommandExecutors.java
@@ -1,16 +1,77 @@
 package com.majornick.ccsh.builtin;
 
+import com.majornick.ccsh.Shell;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 
 public class CommandExecutors {
-    public static void execute_ls(String[] args) {
-        File curDirectory = new File(".");
-        File[] files = curDirectory.listFiles();
+    public static void execute_ls(Shell sh, String[] args) {
+        File[] files = sh.getCurrentDir().listFiles();
         if (files == null) {
             return;
         }
         for (File f : files) {
-            System.out.print(f.getName() + "\t");
+            System.out.printf("%s\t", f.getName());
         }
+        System.out.println();
+    }
+
+    public static void execute_pwd(Shell sh) {
+
+        try {
+            System.out.println(sh.getCurrentDir().getCanonicalPath());
+        } catch (IOException e) {
+            System.err.println("can't get Canonical Path");
+        }
+    }
+
+    public static void execute_cd(Shell sh, String[] args) {
+        if (args.length != 2) {
+            System.out.println("wrong arguments");
+        }
+        File curDir = sh.getCurrentDir();
+        String dir = args[1];
+        File newDir = getExactName(curDir, dir);
+        if (newDir.isDirectory()) {
+            sh.setCurrentDir(newDir);
+            return;
+        }
+        if (newDir.isFile()) {
+            System.out.println("Not a directory");
+            return;
+        }
+        System.out.println("No such file or directory");
+    }
+
+    public static void execute_cat(Shell shell, String[] args) {
+        File curDir = shell.getCurrentDir();
+        if (args.length != 2) {
+            System.out.println("wrong number of arguments");
+        }
+        String fileName = args[1];
+        File file = getExactName(curDir, fileName);
+        if (!file.isFile()) {
+            System.out.println("No such file or directory");
+            return;
+        }
+        try {
+            String content = Files.readString(file.getAbsoluteFile().toPath());
+            System.out.println(content);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static File getExactName(File curDir, String fileName) {
+        File file;
+        if (fileName.startsWith("/")) {
+            return new File(fileName);
+        } else {
+            return new File(String.format("%s/%s", curDir.toString(), fileName));
+        }
+
     }
 }
