0f89ff34b0c6ee845f77325093a7e423e39105a8
MajorNick <nika.chigladze11@gmail.com>
2024-05-05 20:47:37 +0400

    8th challenge: Bulk-String, Simple-String Serialization & Deserialization Done

--- redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/RedisServer.java
+++ redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/RedisServer.java
@@ -4,7 +4,7 @@ import com.majornick.redisserver_challenge8.message.Message;
 import com.majornick.redisserver_challenge8.message.Type;
 
 public class RedisServer {
-
+    private static final String TERMINATOR = "\r\n";
     public static Message deserializeMessage(String message) {
         if (message.isEmpty()) {
             return null;
@@ -18,22 +18,52 @@ public class RedisServer {
         return result;
     }
 
+    public static String serializeMessage(Message message) {
+        String res = null;
+        switch (message.getType()) {
+            case BULK_STRING -> res = serializeBulkString(message);
+            case SIMPLE_STRING -> res = serilizeSimpleString(message);
+        }
+        return res;
+    }
+
+    private static String serializeBulkString(Message message) {
+        StringBuilder res = new StringBuilder();
+        res.append("$");
+        if (message.getContent() == null) {
+            return res.append(-1)
+                    .append(TERMINATOR)
+                    .toString();
+        }
+        return res.append(message.getContent().length())
+                .append(TERMINATOR)
+                .append(message.getContent())
+                .append(TERMINATOR)
+                .toString();
+    }
 
     private static Message deserializeBulkString(String message) {
         Message result = new Message();
         result.setType(Type.BULK_STRING);
-        int grandLine = message.indexOf("\r\n");
+        int grandLine = message.indexOf(TERMINATOR);
         int len = Integer.parseInt(message.substring(1, grandLine));
         if (len == -1) {
             result.setContent(null);
         } else {
-            result.setContent(message.substring(grandLine + 2, message.lastIndexOf("\r\n")));
+            result.setContent(message.substring(grandLine + 2, message.lastIndexOf(TERMINATOR)));
         }
         return result;
     }
 
+    private static String serilizeSimpleString(Message message) {
+        StringBuilder res = new StringBuilder();
+        return res.append("+").append(message.getContent()).append(TERMINATOR).toString();
+    }
     private static Message deserializeSimpleString(String message) {
-        return new Message();
+        Message result = new Message();
+        result.setType(Type.SIMPLE_STRING);
+        result.setContent(message.substring(1, message.lastIndexOf(TERMINATOR)));
+        return result;
     }
 
 
--- redis-server_challenge8/src/test/java/com/majornick/redisserver_challenge8/RedisServerTest.java
+++ redis-server_challenge8/src/test/java/com/majornick/redisserver_challenge8/RedisServerTest.java
@@ -2,38 +2,104 @@ package com.majornick.redisserver_challenge8;
 
 import com.majornick.redisserver_challenge8.message.Message;
 import com.majornick.redisserver_challenge8.message.Type;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RedisServerTest {
 
+
+    //Bulk String TESTS
     @Test
     public void serverNullBulkStringMessageDeserializationTest() {
         Message expected = new Message(Type.BULK_STRING, null);
         Message actual = RedisServer.deserializeMessage("$-1\r\n");
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void serverEmptyBulkStringMessageDeserializationTest() {
         Message expected = new Message(Type.BULK_STRING, "");
         Message actual = RedisServer.deserializeMessage("$0\r\n\r\n");
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void serverBulkStringMessageDeserializationTest() {
         Message expected = new Message(Type.BULK_STRING, "hello");
         Message actual = RedisServer.deserializeMessage("$5\r\nhello\r\n");
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void serverBulkStringWithEndlnMessageDeserializationTest() {
+    public void serverBulkStringWithEndlineMessageDeserializationTest() {
         Message expected = new Message(Type.BULK_STRING, "Hello\r\nWorld");
         Message actual = RedisServer.deserializeMessage("$5\r\nHello\r\nWorld\r\n");
-        Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void serverNullBulkStringMessageSerializationTest() {
+        Message expected = new Message(Type.BULK_STRING, null);
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverEmptyBulkStringMessageSerializationTest() {
+        Message expected = new Message(Type.BULK_STRING, "");
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverBulkStringMessageSerializationTest() {
+        Message expected = new Message(Type.BULK_STRING, "hello");
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverBulkStringWithEndlineMessageSerializationTest() {
+        Message expected = new Message(Type.BULK_STRING, "Hello\r\nWorld");
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
+
+    // Simple STRING
+
+    @Test
+    public void serverEmptySimpleStringMessageDeserializationTest() {
+        Message expected = new Message(Type.SIMPLE_STRING, "");
+        Message actual = RedisServer.deserializeMessage("+\r\n");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverSimpleStringMessageDeserializationTest() {
+        Message expected = new Message(Type.SIMPLE_STRING, "hello");
+        Message actual = RedisServer.deserializeMessage("+hello\r\n");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverEmptySimpleStringMessageSerializationTest() {
+        Message expected = new Message(Type.SIMPLE_STRING, "");
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serverSimpleStringMessageSerializationTest() {
+        Message expected = new Message(Type.SIMPLE_STRING, "hello");
+        String serialized = RedisServer.serializeMessage(expected);
+        Message actual = RedisServer.deserializeMessage(serialized);
+        assertEquals(expected, actual);
+    }
 
 }
