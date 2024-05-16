1e512b18381cbe5917d6a532408dcb590200e1ae
MajorNick <nika.chigladze11@gmail.com>
2024-05-16 15:55:26 +0400

    8th challenge: added Set And Get Cmd

--- redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/client/ClientHandler.java
+++ redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/client/ClientHandler.java
@@ -1,13 +1,19 @@
 package com.majornick.redisserver_challenge8.client;
 
+import com.majornick.redisserver_challenge8.RespDeSerializer;
+import com.majornick.redisserver_challenge8.message.Message;
+import com.majornick.redisserver_challenge8.message.Type;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ClientHandler implements Runnable {
     private final Socket socket;
+    private static HashMap<String, String> DB = new HashMap<>();
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
@@ -31,10 +37,43 @@ public class ClientHandler implements Runnable {
 
     private void execCommand(String[] args, PrintWriter printer) {
         String cmd = args[0];
+        cmd = cmd.toUpperCase();
         switch (cmd) {
-            case "PING":
-                printer.write("PONG");
-                break;
+            case "PING" -> printer.write("PONG");
+            case "ECHO" -> printer.write(args[1]);
+            case "SET" -> printer.write(RespDeSerializer.serializeMessage(executeSetCmd(args)));
+            case "GET" -> printer.write(RespDeSerializer.serializeMessage(executeGetCmd(args)));
         }
     }
+
+    private Message executeGetCmd(String[] args) {
+        if (args.length != 3) {
+            return Message
+                    .builder()
+                    .type(Type.SIMPLE_ERROR)
+                    .content(String.format("wrong number of arguments. expected 3 got %d", args.length))
+                    .build();
+        }
+        return Message.builder()
+                .type(Type.BULK_STRING)
+                .content(DB.get(args[1]))
+                .build();
+    }
+
+    private Message executeSetCmd(String[] args) {
+        if (args.length != 3) {
+            return Message
+                    .builder()
+                    .type(Type.SIMPLE_ERROR)
+                    .content(String.format("wrong number of arguments. expected 3 got %d", args.length))
+                    .build();
+        }
+        DB.put(args[1], args[2]);
+        return Message
+                .builder()
+                .type(Type.SIMPLE_STRING)
+                .content("OK")
+                .build();
+    }
+
 }
--- redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/message/Message.java
+++ redis-server_challenge8/src/main/java/com/majornick/redisserver_challenge8/message/Message.java
@@ -1,45 +1,19 @@
 package com.majornick.redisserver_challenge8.message;
 
+import lombok.*;
+
 import java.util.List;
 import java.util.Objects;
 
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
     private Type type;
     private String content;
     private List<Message> messages;
-
-    public Message(Type type, String content) {
-        this.type = type;
-        this.content = content;
-    }
-
-
-    public Message() {
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
 
     @Override
     public boolean equals(Object o) {
